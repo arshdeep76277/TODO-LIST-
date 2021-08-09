c63f47561dbdea5a1d00ca83a15cdabd8858c80e
Arshdeep Singh <arsh76278@gmail.com>
2021-08-10 01:40:55 +0530

    updated path

--- routes/api/items.js
+++ routes/api/items.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 
 //Item model
-const Item=require('../../models/items.js');
+const Item=require('../../models/Items.js');
 
 router.get('/',(req,res)=>{
     Item.find()
